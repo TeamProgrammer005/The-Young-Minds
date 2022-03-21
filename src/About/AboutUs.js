2669a0637e54f8b532986f88614cefdb4ffd7b33
Harsh Singh <ym.programmer005@gmail.com>
2022-03-21 22:06:42 +0530

    Fixed issus

--- src/About/AboutUs.js
+++ src/About/AboutUs.js
@@ -1,6 +1,6 @@
 import React from 'react'
 import { useState, useEffect } from 'react'
-import  reviewsData  from "./reviewsData.json"
+import reviewsData from "./reviewsData.json"
 import "./AboutUs.css"
 
 export default function AboutUs() {
@@ -15,7 +15,7 @@ export default function AboutUs() {
         {
             name: "Pradyuma Agarwal",
             para: "Beyond the sense of community and opportunity that we strive to provide, XYZ is also the place to find out about amazing design resources and how those at the top of the industry get things done through our podcast, interviews, articles, and live events.",
-            post: "president",
+            post: "leader",
             image: "img/young minds watermark 1.png",
             id: 2
         }
@@ -44,14 +44,14 @@ export default function AboutUs() {
     }, [])
 
     function auto() {
-        slideInterval = setInterval(nextSlide, 5000);
+        slideInterval = setInterval(nextSlide, 10000);
     }
 
     function autoReview() {
-        reviewInterval = setInterval(nextReview, 8000);
+        reviewInterval = setInterval(nextReview, 10000);
     }
 
-    const trying = (index) => {
+    const togglePage = (index) => {
         setCurrentSlide(index);
         setCurrentName(currentName === "Minds" ? "Leader" : "Minds");
     }
@@ -72,7 +72,7 @@ export default function AboutUs() {
             auto();
         }
         return () => clearInterval(slideInterval)
-    }, [currentSlide,currentName])
+    }, [currentSlide, currentName])
 
     return (
         <>
@@ -80,17 +80,18 @@ export default function AboutUs() {
                 <div className="introductionAbout">
                     <div className="navigation">
                         <ul>
-                            <li><i className="fas fa-solid fa-bars utility"></i></li>
+                            {/* <li><i className="fas fa-solid fa-bars utility"></i></li> */}
                             <li><h2>Young Minds</h2></li>
+                            
                         </ul>
                         <div className="user">
-                            <i className="fas fa-solid fa-user utility"></i>
+                           <img src="img/young minds watermark 1.png" alt="" />
                         </div>
                     </div>
                     <span>Welcome Back!</span>
-                    <div className="section">
+                    <div className="sectionAbout">
                         <img src="img/about.png" alt="" />
-                        <div className="content">
+                        <div className="contentAbout">
                             <h3 className='title'>About us</h3>
                             <div className="midHeading">
                                 <h4>The Heart Of</h4>
@@ -137,7 +138,7 @@ export default function AboutUs() {
                         <div className="btnDOM">
                             {Array.from({ length: YMDataLength }).map((item, index) => {
                                 return (
-                                    <div className={currentSlide === index ? "btn active" : "btn"} onClick={() => trying(index)}></div>
+                                    <div className={currentSlide === index ? "btn active" : "btn"} onClick={() => togglePage(index)}></div>
                                 )
                             })}
                         </div>
@@ -148,7 +149,8 @@ export default function AboutUs() {
                         <h2>Why Young Minds</h2>
                         <div className="logo">
                             <img src="img/young minds watermark 1.png" alt="" />
-                            <p>Beyond the sense of community and opportunity that we strive to provide, XYZ is also the place to find out about amazing design resources and how those at the top of the industry get things done through our podcast, interviews, articles, and live events.</p>
+                            <p>We as an organization are driven towards making education equal and accessible for all, our mission is derived from the Sustainable Development Goals set by The United Nations for 2030. Our core activities involve providing quality education, innovation in industry and infrastructure and partnering with the people to achieve our goal.
+                                It is time for disruption in the education sector our visons run together with government launched Atal Innovation Mission, which aims at making S.T.E.A.M Education the core of Indian Education system in the coming years. Therefore, we as team work towards making a platform where free knowledge can be shared with the world, and bring the next generation of learning to the schools and colleges of India</p>
                         </div>
                     </div>
                     <div className="studentCount">
@@ -171,17 +173,19 @@ export default function AboutUs() {
                         <div className="reviewsBox">
                             {reviewsData.map((review, index) => {
                                 return (
-                                    <div className={index === currentReview ? "reviews present" : "reviews"} key={index}>
+                                    <div className={index === currentReview ? "reviews present" : "reviews"} key={review.key}>
                                         {index === currentReview && (
                                             <>
-                                                <div className="empty margin"></div>
+                                                <div className="empty profile">
+                                                <i className="fas fa-solid fa-user utility"></i>
+                                                </div>
                                                 <div className="detail">
                                                     <h4>{review.name}</h4>
                                                     <span>{review.designation}</span>
                                                     <div className="rating">
                                                         {Array.from({ length: review.star }).map(item => {
                                                             return (
-                                                              <i className="fas fa-star utility"></i>
+                                                                <i className="fas fa-star utility"></i>
                                                             )
                                                         })}
 
@@ -206,6 +210,29 @@ export default function AboutUs() {
                             })}
                         </div>
                     </div>
+                </div>
+                <div className="footer">
+                    <ul className="navigate">
+                        <li><a href="/">Home</a></li>
+                        <li><a href="/">About Us</a></li>
+                        <li><a href="/">Serivces</a></li>
+                        <li><a href="/">Our store</a></li>
+                        <li><a href="/">Term's & condition</a></li>
+                    </ul>
+                    <div className="socialMedia">
+                        <h4>Follow Us</h4>
+                    <ul className='socialMediaIcons'>
+                        <li><a href="/"><i className="fas fa-envelope utility"></i></a></li>
+                        <li><a href="/"><i className="fab fa-yahoo utility"></i></a></li>
+                        <li><a href="/"><i className="fab fa-linkedin-in utility"></i></a></li>
+                        <li><a href="/"><i className="fab fa-apple utility"></i></a></li>
+                    </ul>
+                    </div>
+                    <div className="shortIntro">
+                        <h4>The Young Minds</h4>
+                        <img src="img/young minds watermark 1.png" alt="logo" />
+                    </div>
+                <span className='copyright'>&copy; 2022 Theyoungminds all rights reserved.</span>
                 </div>
             </div>
         </>
